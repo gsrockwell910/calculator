1c2bf76c4521fa7e2f209a33782be27628b22edc
Gavin Rockwell <gsrockwell910@gmail.com>
2023-01-31 23:49:30 -0500

    Create all global variables for the calculator

---
+++ script.js
@@ -0,0 +1,7 @@
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operation]");
+const deleteButton = document.querySelector("[data-delete]");
+const allClearButton = document.querySelector("[data-all-clear]");
+const equalsButton = document.querySelector("[data-equals]");
+const currentOperandText = document.querySelector("[data-current-operand]");
+const previousOperandText = document.querySelector("[data-previous-operand]");
