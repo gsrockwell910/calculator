4f50268343a4956aba2d9670d9436a227b480caf
Gavin Rockwell <gsrockwell910@gmail.com>
2023-01-31 23:55:00 -0500

    Add calculator class and constructor with all necessary components for the calculator

--- script.js
+++ script.js
@@ -5,3 +5,40 @@ const allClearButton = document.querySelector("[data-all-clear]");
 const equalsButton = document.querySelector("[data-equals]");
 const currentOperandText = document.querySelector("[data-current-operand]");
 const previousOperandText = document.querySelector("[data-previous-operand]");
+
+class Calculator {
+    constructor(currentOperandText, previousOperandText) {
+        this.currentOperandText = currentOperandText;
+        this.previousOperandText = previousOperandText;
+        this.clearAll();
+     };
+
+     delete() {
+
+     }
+
+     clearAll() {
+
+     }
+
+     displayNumber(number) { 
+
+     } 
+
+     computeExpression() {
+
+     }
+
+      pickOperation(operation) {
+
+      }
+
+      getDisplayNumber(number) {
+
+      }
+
+      update() {
+        
+      }
+
+};
